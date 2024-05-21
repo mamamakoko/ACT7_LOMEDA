f0a37611c6ff9ac50671b583a9c03f94af12463d
John Francis Lomeda <kokoylemonada@gmail.com>
2024-05-21 15:05:31 +0800

    Create

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = "Product list form in ProductController";
+        return view('products.index', ['products' => $products]);
+    }
+
+    public function create() {
+        return view('products.create');
+    }
 }
